f36d459b5fed4bbb8644a9f7478ad7ad43800971
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-04-06 02:41:29 -0700

    Added aliases for relationships in models table and included them in the controller routes when including models of foreign keys

--- models/index.js
+++ models/index.js
@@ -1,52 +1,61 @@
-const TBLMessages = require('./TBLMessages');
-const TBLRole = require('./TBLRole');
-const TBLScript = require('./TBLScript');
-const TBLSearchHistory = require('./TBLSearchHistory');
-const TBLUser = require('./TBLUser');
+const TBLMessages = require("./TBLMessages");
+const TBLRole = require("./TBLRole");
+const TBLScript = require("./TBLScript");
+const TBLSearchHistory = require("./TBLSearchHistory");
+const TBLUser = require("./TBLUser");
 
-TBLUser.belongsTo(TBLRole,{
-  foreignKey: 'roleID',
+TBLUser.belongsTo(TBLRole, {
+  foreignKey: "roleID",
   onDelete: "SET NULL",
 });
 
-TBLScript.belongsTo(TBLUser,{
-  foreignKey: 'authorID',
+TBLScript.belongsTo(TBLUser, {
+  foreignKey: "authorID",
   onDelete: "SET NULL",
 });
 
-TBLScript.belongsTo(TBLUser,{
-  foreignKey: 'assignedTo',
+TBLScript.belongsTo(TBLUser, {
+  foreignKey: "authorID",
+  as: "Author",
+  onDelete: "SET NULL",
+});
+
+TBLScript.belongsTo(TBLUser, {
+  foreignKey: "assignedTo",
+  as: "Assignee",
   onDelete: "SET NULL",
 });
 
 TBLMessages.belongsTo(TBLUser, {
-  foreignKey: 'receiverID',
+  foreignKey: "receiverID",
   onDelete: "SET NULL",
 });
 
 TBLMessages.belongsTo(TBLUser, {
-  foreignKey: 'senderID',
+  foreignKey: "senderID",
   onDelete: "SET NULL",
 });
 
 TBLRole.hasMany(TBLUser, {
-  foreignKey: 'roleID',
+  foreignKey: "roleID",
 });
 
 TBLUser.hasMany(TBLScript, {
-  foreignKey: 'authorID',
+  foreignKey: "authorID",
+  as: "AuthoredScripts",
 });
 
 TBLUser.hasMany(TBLScript, {
-  foreignKey: 'assignedTo',
+  foreignKey: "assignedTo",
+  as: "AssignedScripts",
 });
 
 TBLUser.hasMany(TBLMessages, {
-  foreignKey: 'receiverID',
+  foreignKey: "receiverID",
 });
 
 TBLUser.hasMany(TBLMessages, {
-  foreignKey: 'senderID',
+  foreignKey: "senderID",
 });
 
-module.exports = { TBLMessages, TBLRole, TBLScript, TBLSearchHistory, TBLUser};
+module.exports = { TBLMessages, TBLRole, TBLScript, TBLSearchHistory, TBLUser };
