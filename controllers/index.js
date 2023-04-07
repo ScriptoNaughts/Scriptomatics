0699b90b886be4d70efc8fec0668380a3be9ce04
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-04-07 00:45:23 -0700

    Separated routes into client and developer routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 
-const apiRoutes = require("./api");
+const apiRoutes = require("./api"); // for client-facing routes
+const developerRoutes = require("./dev") // for backend developer routes
 const homeRoutes = require("./home-routes.js");
 
+// home-routes have the prefix "/"
 router.use("/", homeRoutes);
+// client routes (routes that are requested by the client) have the prefix "/api"
 router.use("/api", apiRoutes);
+// developer routes (routes not used by clients) which are used for backend management & testing have the prefix "/dev" 
+router.use("/dev",developerRoutes)
 
 module.exports = router;
