bf5569032b229aa19be311e0a5150aa6067b0a0b
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-04-11 13:37:29 -0700

    Fixed writer's posted scripts page

--- controllers/dev/index.js
+++ controllers/dev/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
+const scriptRoutes = require("./script-routes")
 
 router.use("/users", userRoutes);
+router.use("/scripts", scriptRoutes);
 
 module.exports = router;
