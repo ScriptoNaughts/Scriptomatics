4b86a5044c0a906365f0257ef0385fb92ba5e655
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-04-10 21:55:12 -0700

    Fixed workspace page not loading properly

--- public/js/draft-card.js
+++ public/js/draft-card.js
@@ -1,24 +1,27 @@
 var editButtonEl = document.querySelector(".edit-button");
-var titleTextBox = document.querySelector(".script-title");
-var descTextBox = document.querySelector(".script-description");
-var scriptTextBox = document.querySelector(".script-title");
+var titleTextBox = document.querySelector("#script-title");
+var descTextBox = document.querySelector("#script-description");
+var scriptTextBox = document.querySelector("#script-text");
 
 var saveButtonHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    fetch(`/api/scripts/edit/${event.target.getAttribute("data-id")}`)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-              response.title
-              response.description
-              response.text
-          });
-        } else {
-          alert("Error: " + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert("Unable to connect to GitHub");
-      });
-}
+  fetch(`/api/scripts/edit/${event.target.getAttribute("data-id")}`)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log("\n\nResponse:" + JSON.stringify(data) + "\n\n");
+          titleTextBox.value = data.title;
+          descTextBox.value = data.description;
+          scriptTextBox.value = data.text;
+        });
+      } else {
+        alert("\n\nError: " + data.statusText);
+      }
+    })
+    .catch(function (error) {
+      console.log("\n\nError" + error);
+    });
+};
+
+editButtonEl.addEventListener("click", saveButtonHandler);
